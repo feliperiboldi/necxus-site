9e4009c1cb2790e3b8329352e6f66783df192ad7
Felipe Riboldi <feliperiboldimarques@outlook.com>
2019-01-10 15:44:01 -0200

    Front-End - Carousel, História, Serviços e Contato

---
+++ dist/js/owl.js
@@ -0,0 +1,40 @@
+$('.owl-banner').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    animateIn: 'fadeInLeft',
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+//
+
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+})
