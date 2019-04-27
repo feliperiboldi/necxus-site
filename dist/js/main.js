b7db2cf32b7a12a42913bb283049395d98f90cfe
Felipe Riboldi <feliperiboldimarques@outlook.com>
2019-04-27 20:17:18 -0300

    Script do Menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,3 +9,24 @@ if($(window).width() > 991) {
         $('.carousel-image').height($(window).height() - 0);
     });
 }
+
+
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 100) {
+            $(".main-menu").addClass("menu-on-scroll");
+        } else {
+            $(".main-menu").removeClass("menu-on-scroll");
+        }
+    });
+});
+
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 100) {
+            $(".main-menu").addClass("menu-link-white");
+        } else {
+            $(".main-menu").removeClass("menu-link-black");
+        }
+    });
+});
